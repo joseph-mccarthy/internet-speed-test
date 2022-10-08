d9219aac7852b8caaf19fd37506919201f187e2d
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2022-10-08 12:58:55 +0100

    refactor into class

--- speedtest/main.py
+++ speedtest/main.py
@@ -1,37 +1,41 @@
+from cProfile import run
 import speedtest
 import paho.mqtt.client as paho
 import json
 import logging
 
-def send_message(payload):
-        broker = "broker"
-        topic = "application/speedtest"
+class SpeedTestRunner:
 
-        client = paho.Client(client_id="speedtest_runner")
-        client.connect(broker)
-        client.publish(topic, payload)
+    def __send_message(self,payload):
+            broker = "broker"
+            topic = "application/speedtest"
 
-def main():
-    logging.basicConfig(level=logging.DEBUG)
-    logging.info("running internet speed test")
-    download,upload = run_test()
-    logging.info("sending mqtt message")
-    send_message(generate_payload(download,upload))
+            client = paho.Client(client_id="speedtest_runner")
+            client.connect(broker)
+            client.publish(topic, payload)
 
-def generate_payload(download:float, upload:float):
-    payload = json.dumps({
-                "upload": upload,
-                "download": download,
-            })
-    return payload
+    def run(self):
+        logging.info("running internet speed test")
+        download,upload = self.__run_test()
+        logging.info("sending mqtt message")
+        self.__send_message(self.__generate_payload(download,upload))
 
-def run_test():
-    st = speedtest.Speedtest(secure=True)
-    download_speed = "{:.2f}".format(st.download() / 1000000)
-    logging.info(f"download speed is: {download_speed} Mbps")
-    upload_speed = "{:.2f}".format(st.upload() / 1000000)
-    logging.info(f"upload speed is: {upload_speed} Mbps")
-    return (download_speed,upload_speed)
+    def __generate_payload(self,download:float, upload:float):
+        payload = json.dumps({
+                    "upload": upload,
+                    "download": download,
+                })
+        return payload
+
+    def __run_test(self):
+        st = speedtest.Speedtest(secure=True)
+        download_speed = "{:.2f}".format(st.download() / 1000000)
+        logging.info(f"download speed is: {download_speed} Mbps")
+        upload_speed = "{:.2f}".format(st.upload() / 1000000)
+        logging.info(f"upload speed is: {upload_speed} Mbps")
+        return (download_speed,upload_speed)
 
 if __name__ == "__main__":
-    main()
+    logging.basicConfig(level=logging.INFO)
+    runner = SpeedTestRunner()
+    runner.run()
