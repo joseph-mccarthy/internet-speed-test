e1166d6f7eb95d3c1b5d2b41b6e94ef8db0cf9af
Joseph McCarthy <joseph-mccarthy@users.noreply.github.com>
2022-10-08 13:00:13 +0100

    remove import, added pep8 and format code to standards

--- speedtest/main.py
+++ speedtest/main.py
@@ -1,30 +1,30 @@
-from cProfile import run
 import speedtest
 import paho.mqtt.client as paho
 import json
 import logging
 
+
 class SpeedTestRunner:
 
-    def __send_message(self,payload):
-            broker = "broker"
-            topic = "application/speedtest"
+    def __send_message(self, payload):
+        broker = "broker"
+        topic = "application/speedtest"
 
-            client = paho.Client(client_id="speedtest_runner")
-            client.connect(broker)
-            client.publish(topic, payload)
+        client = paho.Client(client_id="speedtest_runner")
+        client.connect(broker)
+        client.publish(topic, payload)
 
     def run(self):
         logging.info("running internet speed test")
-        download,upload = self.__run_test()
+        download, upload = self.__run_test()
         logging.info("sending mqtt message")
-        self.__send_message(self.__generate_payload(download,upload))
+        self.__send_message(self.__generate_payload(download, upload))
 
-    def __generate_payload(self,download:float, upload:float):
+    def __generate_payload(self, download: float, upload: float):
         payload = json.dumps({
-                    "upload": upload,
-                    "download": download,
-                })
+            "upload": upload,
+            "download": download,
+        })
         return payload
 
     def __run_test(self):
@@ -33,7 +33,8 @@ class SpeedTestRunner:
         logging.info(f"download speed is: {download_speed} Mbps")
         upload_speed = "{:.2f}".format(st.upload() / 1000000)
         logging.info(f"upload speed is: {upload_speed} Mbps")
-        return (download_speed,upload_speed)
+        return (download_speed, upload_speed)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
